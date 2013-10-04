3c9a1ac14897a6332ae43d6005feb6797485d3d6
Besnik Br <besnik.br@googlemail.com>
2013-10-04 20:45:26 +0200

    added more functions is_mobile, is_tablet and is_* (* = device name, e.g. is_samsung)

--- src/Bes/Twig/Extension/MobileDetectExtension.php
+++ src/Bes/Twig/Extension/MobileDetectExtension.php
@@ -7,9 +7,12 @@ class MobileDetectExtension extends \Twig_Extension
 {
     protected $detector;
 
+    /**
+     * Constructor
+     */
     public function __construct()
     {
-        $this->detector = new Mobile_Detect();
+        $this->detector = new \Mobile_Detect();
     }
 
     /**
@@ -19,9 +22,71 @@ class MobileDetectExtension extends \Twig_Extension
      */
     public function getFunctions()
     {
-        return array(
-            'is_mobile' => new \Twig_Function_Method($this->detector, 'isMobile'),
+        $functions = array(
+            'get_available_devices' => new \Twig_Function_Method($this, 'getAvailableDevices'),
+            'is_mobile' => new \Twig_Function_Method($this, 'isMobile'),
+            'is_tablet' => new \Twig_Function_Method($this, 'isTablet'),
         );
+
+        foreach ($this->getAvailableDevices() as $device => $fixedName) {
+            $methodName = 'is'.$device;
+            $twigFunctionName = 'is_'.$fixedName;
+            $functions[$twigFunctionName] = new \Twig_Function_Method($this, 'is'.$methodName);
+        }
+
+        return $functions;
+    }
+
+    /**
+     * Returns an array of all available devices
+     *
+     * @return array
+     */
+    public function getAvailableDevices()
+    {
+        $availableDevices = array();
+        $rules = array_change_key_case($this->detector->getRules());
+
+        foreach ($rules as $device => $rule) {
+            $availableDevices[$device] = static::fromCamelCase($device);
+        }
+
+        return $availableDevices;
+    }
+
+    /**
+     * Pass through calls of undefined methods to the mobile detect library
+     *
+     * @param $name
+     * @param $arguments
+     * @return mixed
+     */
+    public function __call($name, $arguments)
+    {
+        return call_user_func_array(array($this->detector, $name), $arguments);
+    }
+
+    /**
+     * Converts a string to camel case
+     *
+     * @param $string
+     * @return mixed
+     */
+    protected static function toCamelCase($string)
+    {
+        return preg_replace('~\s+~', '', lcfirst(ucwords(strtr($string, '_', ' '))));
+    }
+
+    /**
+     * Converts a string from camel case
+     *
+     * @param $string
+     * @param string $separator
+     * @return string
+     */
+    protected static function fromCamelCase($string, $separator = '_')
+    {
+        return strtolower(preg_replace('/(?!^)[[:upper:]]+/', $separator.'$0', $string));
     }
 
     /**
